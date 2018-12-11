a04886dca1ae84b0e13a2415145a3e107e1b056e
Nihit <codingnihit@gmail.com>
2018-12-11 10:01:46 +0545

    pagination blog site

--- posts/models.py
+++ posts/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.urls import reverse #absolute url
 
 
-#def upload_location(post,filename):
-	#return"%s%s" %(post.id, filename)
+def upload_location(instance,filename):
+	return"%s%s" %(instance.id, filename)
 
 
 class Post(models.Model):
 	title=models.CharField(max_length=50)
-	image=models.FileField(null=True,blank=True)
-	#image=models.ImageField(upload_to=upload_location,null=True,blank=True,width_field="width_field",height_field="height_field")
-	#height_field=models.IntegerField(default=0)
-	#width_field=models.IntegerField(default=0)
+	#image=models.FileField(null=True,blank=True)
+	image=models.ImageField(upload_to=upload_location,null=True,blank=True,width_field="width_field",height_field="height_field")
+	height_field=models.IntegerField(default=0)
+	width_field=models.IntegerField(default=0)
 	content=models.TextField(max_length=200)
 	updated=models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp=models.DateTimeField(auto_now=False, auto_now_add=True)
